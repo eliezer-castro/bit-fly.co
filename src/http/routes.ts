cf1d554f05dbe29933c27d309413d00ccae96e9b
eliezer <eliezergino@gmail.com>
2024-05-06 15:38:33 -0300

    feat: Update routes in appRoutes function

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -11,11 +11,7 @@ import { authenticate } from './middleware/authMiddleware'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/api/v1/shorten-url', { preHandler: authenticate }, createShortUrl)
-  app.get(
-    '/api/v1/:shortCode',
-    { preHandler: authenticate },
-    redirectToOriginalUrl,
-  )
+  app.get('/api/v1/:shortCode', redirectToOriginalUrl)
   app.get(
     '/api/v1/user/:userId/urls',
     { preHandler: authenticate },
@@ -26,11 +22,7 @@ export async function appRoutes(app: FastifyInstance) {
     { preHandler: authenticate },
     getShortUrlDetails,
   )
-  app.delete(
-    '/apii/v1/delete-url',
-    { preHandler: authenticate },
-    deleteShortUrl,
-  )
+  app.delete('/api/v1/delete-url', { preHandler: authenticate }, deleteShortUrl)
   app.post('/api/v1/register', registerUser)
   app.post('/api/v1/login', loginUser)
 }
