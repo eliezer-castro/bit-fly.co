0a5ea020b46aa669fa19be69d28c6849d83c2c2a
eliezer <eliezergino@gmail.com>
2024-05-24 10:40:37 -0300

    refactor: Update UniqueShortenedURLGenerator to use ShortenedUrlRepository

--- src/services/generateUniqueShortenedURL.ts
+++ src/services/generateUniqueShortenedURL.ts
@@ -1,48 +1,46 @@
-import { PrismaClient } from '@prisma/client'
 import crypto from 'crypto'
+import { ShortenedUrlRepository } from '@/repositories/shortened-url-repository'
 
-const prisma = new PrismaClient()
+export class UniqueShortenedURLGenerator {
+  // eslint-disable-next-line prettier/prettier
+  constructor(private shortenedUrlRepository: ShortenedUrlRepository) { }
 
-function generateRandomHash(hashLength: number): string {
-  const allowedCharacters =
-    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-  let hash = ''
-  for (let i = 0; i < hashLength; i++) {
-    const randomIndex = Math.floor(
-      (crypto.randomBytes(1)[0] / 256) * allowedCharacters.length,
-    )
-    hash += allowedCharacters[randomIndex]
+  private generateRandomHash(hashLength: number): string {
+    const allowedCharacters =
+      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    let hash = ''
+    for (let i = 0; i < hashLength; i++) {
+      const randomIndex = Math.floor(
+        (crypto.randomBytes(1)[0] / 256) * allowedCharacters.length,
+      )
+      hash += allowedCharacters[randomIndex]
+    }
+    return hash
   }
-  return hash
-}
 
-async function isHashUnique(hash: string) {
-  try {
-    const existingShortenedUrl = await prisma.shortenedUrl.findUnique({
-      where: {
-        short_url: hash,
-      },
-    })
-    return existingShortenedUrl === null
-  } catch (error) {
-    console.error('Error querying the database: ', error)
-    throw error
-  }
-}
-
-export async function generateUniqueShortenedURL(
-  retries = 10,
-): Promise<string> {
-  const hashLength = 7
-
-  for (let attempt = 0; attempt < retries; attempt++) {
-    const hash = generateRandomHash(hashLength)
-    if (await isHashUnique(hash)) {
-      return hash
+  private async isHashUnique(hash: string) {
+    try {
+      const existingShortenedUrl =
+        await this.shortenedUrlRepository.findByShortUrl(hash)
+      return existingShortenedUrl === null
+    } catch (error) {
+      console.error('Error querying the database: ', error)
+      throw error
     }
   }
 
-  throw new Error(
-    'Failed to generate unique shortened URL after maximum retries',
-  )
+  public async generateUniqueShortenedURL(retries = 10): Promise<string> {
+    const hashLength = 7
+
+    for (let attempt = 0; attempt < retries; attempt++) {
+      const hash = this.generateRandomHash(hashLength)
+      if (await this.isHashUnique(hash)) {
+        return hash
+      }
+    }
+
+    throw new Error(
+      'Failed to generate unique shortened URL after maximum retries',
+    )
+  }
 }
